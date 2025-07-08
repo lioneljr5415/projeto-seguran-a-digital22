e8a86d324d10de0335a236b340f15262c0281a9c
lioneljr5415 <136764104+lioneljr5415@users.noreply.github.com>
2025-07-08 08:46:10 -0300

    ...

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 document.querySelector('.parametro-senha__botao');
 
 const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
 numeroSenha.textContent = 5;
+
+const botoes = document.querySelector('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+console.log(botoes);
+
+function diminuiTamanho(){
+tamanhoSenha = tamanhoSenha - 1;
+numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentaTamanho() {
+tamanhoSenha = tamanhoSenha + 1;
+numeroSenha.textContent = tamanhoSenha;
+}
